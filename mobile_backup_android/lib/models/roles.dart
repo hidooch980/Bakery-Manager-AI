c38d9f272ab467bc77f41f58552ba2de64b31cbc
hidooch980 <hidooch980@gmail.com>
2026-07-23 14:56:11 +0330

    chore: remove leftover junk files and dead code

--- mobile_backup_android/lib/models/roles.dart
+++
@@ -1,6 +0,0 @@
-class Roles{
-static const admin='ADMIN';
-static const seller='SELLER';
-static const kneader='KNEADER';
-static const divider='DIVIDER';
-}
--- mobile_backup_android/lib/models/user_role.dart
+++
@@ -1,6 +0,0 @@
-enum UserRole{
-manager,
-seller,
-doughMaker,
-divider
-}
